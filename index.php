ec7046767b65e0bc78d3d17dbd665cd3f11610e3
mukta10375510 <36004206+mukta10375510@users.noreply.github.com>
2018-01-31 16:00:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,9 @@ echo "This file is stored in GitHub.";
 
 
 ?>
+<P> This is <BR/>
+  Normal <BR/>
+  HTML<BR/>
+  <image src="https://media-exp2.licdn.com/mpr/mpr/AAEAAQAAAAAAAAJjAAAAJDEwYmNhODU0LTBhY2YtNGM1My04OGU0LWQ3ZTk5OTMzNzFhZg.png">
+    
+    <?php echo "This is from php."?.
